956e9542334203899ee2ca2400a3c2ec2ccc9c23
Davit Abgaryan <davit.abgaryan@the.hell.am>
2025-05-03 17:23:30 +0400

    lab1

--- math.s
+++ math.s
@@ -1,4 +1,4 @@
-# you'll need to replace '?' with more meaningful stuff.
+
 
 .text
 	.globl	math_func
@@ -8,11 +8,11 @@ math_func:
    # this moves variable i from the memory to the %rax register
    mov i, %rax 
    # write something to move variable j to the %rbx register
-   ??????????
+   mov j, %rbx
    # do j - i
-   sub  ?, ? 
+   sub  %rax, %rbx  
    # at the end of the function %rax should contain the return of the function
-   # do something so for that:
-   ??????????
+   # do something so for that
+   mov %rbx, %rax
    #function exits
    ret
